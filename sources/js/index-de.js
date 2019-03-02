24bd06843a0ee70e1e2b1a9bdb7ffabe545151d5
Grzegorz Tomicki <gtomicki@gmail.com>
2019-03-02 12:00:18 +0100

    removing de version

--- sources/js/index-de.js
+++
@@ -1,7 +0,0 @@
-import './modules/foreachPolyfill';
-import './modules/scrolltop';
-import './modules/nav';
-// import './modules/snow';
-
-// import '../../sources/scss/modules/_gallery.scss';
-// import '../../sources/scss/modules/_grid.scss';
