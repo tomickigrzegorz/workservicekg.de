3dd38a15023f433e54a96c2dbb9c8da954d578bd
Grzegorz Tomicki <gtomicki@gmail.com>
2019-05-16 20:24:21 +0200

    fixing svg logo in IE11/10

--- sources/js/modules/activeMenu.js
+++ sources/js/modules/activeMenu.js
@@ -3,16 +3,15 @@ const ActiveMenu = (classes) => {
 
   const menu = document.querySelectorAll(`.${classMenu} a`);
 
-  menu.forEach((item, i) => {
-    item.addEventListener('click', () => {
+  for(let i = 0; i < menu.length; i++) {
+    menu[i].addEventListener('click', () => {
       let current = document.querySelectorAll(`.${classActive}`);
       if (current.length > 0) {
         current[0].className = current[0].className.replace(classActive, '');
       }
-      item.className += classActive;
+      menu[i].className += classActive;
     });
-
-  });
+  }
 };
 
 export default ActiveMenu;
