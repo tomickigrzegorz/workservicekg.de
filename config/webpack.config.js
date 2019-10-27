5b4cf6d34183ca0f4eccce0b830fbc7d0d1022f6
Grzegorz Tomicki <gtomik@gmail.com>
2019-10-27 13:54:57 +0100

    refactoring

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
@@ -42,10 +42,10 @@ module.exports = (env, argv) => {
         minifyCSS: true
       }
     } : {
-      pathToDist: 'dist',
-      mode: 'development',
-      minify: false
-    };
+        pathToDist: 'dist',
+        mode: 'development',
+        minify: false
+      };
 
   const entryHtmlPlugins = Object.keys(ENTRY.html).map(entryName => {
     const fileName = entryName === 'index-de' ? 'index' : 'index-pl';
@@ -72,137 +72,137 @@ module.exports = (env, argv) => {
     output: output,
     module: {
       rules: [{
-          // JS
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader'
+        // JS
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        // HTML
+        test: /\.html$/,
+        use: [{
+          loader: 'html-loader',
+          options: {
+            minimize: argv.mode === 'development' ? false : true
           }
-        },
-        {
-          // HTML
-          test: /\.html$/,
-          use: [{
-            loader: 'html-loader',
-            options: {
-              minimize: argv.mode === 'development' ? false : true
-            }
-          }]
-        },
-        {
-          // CSS SASS SCSS
-          test: /\.(css|sass|scss)$/,
-          use: [
-            argv.mode === 'development' ?
+        }]
+      },
+      {
+        // CSS SASS SCSS
+        test: /\.(css|sass|scss)$/,
+        use: [
+          argv.mode === 'development' ?
             'style-loader' :
             MiniCssExtractPlugin.loader,
-            {
-              loader: 'css-loader',
-              options: {
-                importLoaders: 2,
-                sourceMap: true
-              }
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                sourceMap: true,
-                config: {
-                  path: './config/'
-                }
-              }
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: {
+                path: './config/'
               }
             }
-          ]
-        },
-        {
-          // IMAGES
-          test: /\.(jpe?g|png|gif|svg)$/i,
-          loader: 'file-loader',
-          options: {
-            name: '[name]-[hash:8].[ext]',
-            outputPath: imageModeFileLoader,
-            publicPath: imageModeFileLoader,
-            // publicPath: argv.mode === 'production' ? '../../images/' : '',
-            useRelativePath: true
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
           }
-        },
-        {
-          // PUG
-          test: /\.pug$/,
-          loader: 'pug-loader',
-          options: {
-            pretty: true,
-            self: true
-          }
+        ]
+      },
+      {
+        // IMAGES
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[name]-[hash:8].[ext]',
+          outputPath: imageModeFileLoader,
+          publicPath: imageModeFileLoader,
+          // publicPath: argv.mode === 'production' ? '../../images/' : '',
+          useRelativePath: true
         }
+      },
+      {
+        // PUG
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+          pretty: true,
+          self: true
+        }
+      }
       ]
     },
     optimization: uglifyjsplugin,
     plugins: [
-        prodPlugin(
-          new CleanWebpackPlugin({
-            verbose: true
-          }),
-          argv
-        ),
-        prodPlugin(
-          new MiniCssExtractPlugin({
-            filename: "vendor/css/index.[hash].css",
-          }),
-          argv
-        ),
-        prodPlugin(
-          new SWPrecacheWebpackPlugin({
-            cacheId: 'kg',
-            dontCacheBustUrlsMatching: /\.\w{8}\./,
-            filename: 'sw.js',
-            minify: true,
-            navigateFallback: PUBLIC_PATH + 'index.html',
-            stripPrefix: OUTPUT_DIR,
-            staticFileGlobs: [
-              `${OUTPUT_DIR}/assets/manifest.json`,
-              `${OUTPUT_DIR}/favicon.ico`,
-              `${OUTPUT_DIR}/vendor/js/*.js`,
-              `${OUTPUT_DIR}/images/static/*.png`,
-              `${OUTPUT_DIR}/images/*.jpg`,
-              `${OUTPUT_DIR}/images/*.png`,
-              // `${OUTPUT_DIR}/*.html`,
-              // `${OUTPUT_DIR}/vendor/css/*.css`,
-            ],
-          }),
-          argv
-        ),
-        prodPlugin(
-          new CopyWebpackPlugin([{
-              from: 'sources/assets/',
-              to: 'assets/'
-            },
-            {
-              from: 'sources/images/static/',
-              to: 'images/static'
-            },
-            {
-              from: 'sources/assets/favicon.ico',
-              to: './'
-            },
-            {
-              from: 'sources/assets/.htaccess',
-              to: './'
-            },
-            {
-              from: 'sources/assets/robots.txt',
-              to: './'
-            },
-          ]),
-          argv
-        )
-      ]
+      prodPlugin(
+        new CleanWebpackPlugin({
+          verbose: true
+        }),
+        argv
+      ),
+      prodPlugin(
+        new MiniCssExtractPlugin({
+          filename: "vendor/css/index.[hash].css",
+        }),
+        argv
+      ),
+      prodPlugin(
+        new SWPrecacheWebpackPlugin({
+          cacheId: 'kg',
+          dontCacheBustUrlsMatching: /\.\w{8}\./,
+          filename: 'sw.js',
+          minify: true,
+          navigateFallback: PUBLIC_PATH + 'index.html',
+          stripPrefix: OUTPUT_DIR,
+          staticFileGlobs: [
+            `${OUTPUT_DIR}/assets/manifest.json`,
+            `${OUTPUT_DIR}/favicon.ico`,
+            `${OUTPUT_DIR}/vendor/js/*.js`,
+            `${OUTPUT_DIR}/images/static/*.png`,
+            `${OUTPUT_DIR}/images/*.jpg`,
+            `${OUTPUT_DIR}/images/*.png`,
+            // `${OUTPUT_DIR}/*.html`,
+            // `${OUTPUT_DIR}/vendor/css/*.css`,
+          ],
+        }),
+        argv
+      ),
+      prodPlugin(
+        new CopyWebpackPlugin([{
+          from: 'sources/assets/',
+          to: 'assets/'
+        },
+        {
+          from: 'sources/images/static/',
+          to: 'images/static'
+        },
+        {
+          from: 'sources/assets/favicon.ico',
+          to: './'
+        },
+        {
+          from: 'sources/assets/.htaccess',
+          to: './'
+        },
+        {
+          from: 'sources/assets/robots.txt',
+          to: './'
+        },
+        ]),
+        argv
+      )
+    ]
       .concat(entryHtmlPlugins)
       .concat(
         prodPlugin(
@@ -230,9 +230,9 @@ function devProdOption(dev, prod, argv) {
 }
 
 function prodPlugin(plugin, argv) {
-  return argv.mode === 'production' ? plugin : () => {};
+  return argv.mode === 'production' ? plugin : () => { };
 }
 
 function devPlugin(plugin, argv) {
-  return argv.mode === 'development' ? plugin : () => {};
+  return argv.mode === 'development' ? plugin : () => { };
 }
