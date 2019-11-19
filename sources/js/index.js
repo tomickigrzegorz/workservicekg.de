5a7d4171bcd8256f38ddc368d30da9b8c1b2f03d
Grzegorz Tomicki <gtomik@gmail.com>
2019-11-19 21:39:23 +0100

    update

--- sources/js/index.js
+++ sources/js/index.js
@@ -7,29 +7,28 @@ import MobileMenu from './modules/mobileMenu';
 import ScrollTopButton from './modules/buttonScrolltop';
 import StickyHeader from './modules/stickyHeader';
 
-window.addEventListener('load', () => {
-  // smoothscroll menu and back-to-top
-  const scroll = new SmoothScroll('.scroll a[href*="#"]', {
-    speed: 800,
-    offset: 50,
-  });
-  // add full year to footer
-  documentQuerySelector(classes.classDate).innerHTML = new Date().getFullYear();
+// add full year to footer
+documentQuerySelector(classes.classDate).innerHTML = new Date().getFullYear();
 
-  // show menu and triggered on mobile devices
-  MobileMenu(classes);
+// show menu and triggered on mobile devices
+MobileMenu(classes);
 
-  // add class to click item
-  ActiveMenu(classes);
+// add class to click item
+ActiveMenu(classes);
 
-  // add sticky class to header-container load
+// add sticky class to header-container load
+StickyHeader(classes);
+
+// add scroll button to body
+ScrollTopButton(classes);
+
+// smoothscroll menu and back-to-top
+const scroll = new SmoothScroll('.scroll a[href*="#"]', {
+  speed: 800,
+  offset: 50,
+});
+
+window.addEventListener('scroll', () => {
+  // add sticky class to header-container scroll
   StickyHeader(classes);
-
-  window.addEventListener('scroll', () => {
-    // add sticky class to header-container scroll
-    StickyHeader(classes);
-  });
-
-  // add scroll button to body
-  ScrollTopButton(classes);
 });
